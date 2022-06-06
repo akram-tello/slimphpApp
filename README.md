0b544493942565b84be8ecd1629a6f1f9103ab1a
akram-tello <a.tello@graduate.utm.my>
2022-06-06 12:55:50 +0800

    first init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # slimphpApp
+
+installiton instructions:
+
+```
+1. clone the project
+2. run composer install
+3. add .htaccess file on main root folder
+4. add `RewriteEngine on
+RewriteRule ^$ public/ [L]
+RewriteRule (.*) public/$1 [L]` in the .htaccess file
+5. add another .htaccess file on public folder
+6. add `RewriteEngine on
+RewriteRule ^$ index.php [L]
+RewriteRule (.*) index.php?slim=$1 [L]` in the .htaccess file
+7. run `php -S localhost:8080`
+
+```
---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
+
+$app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, Slim Framework!');
+    return $response;
+})->setName('root');
+
+$app->get('/app', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, app!');
+    return $response;
+})->setName('root');
+
+// Run app
+$app->run();
+?> 
